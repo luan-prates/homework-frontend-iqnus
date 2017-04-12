47962436187af79282e8dda94a925a6eb4185f30
luan-prates <pratesr21@gmail.com>
2017-04-12 16:16:06 -0300

    Montando a nova empresa quando adicionada.
    Mais manipulação de html....

---
+++ frontend/_js/montar-empresa.js
@@ -0,0 +1,22 @@
+function montarEmpresa(nome_empresa, id_dev) {
+  var nova_empresa =
+  '<tr class="tr_empresa">'+
+    '<td class="info-empresa">'+campo_empresa.value+'</td>'+
+    '<td class="info-dev" colspan="6">'+
+      '<table class="tabela_dev" id="'+ nome_empresa +'"'+ 'border="0">'+
+        '<tr id="'+ id_dev +'"' + 'class="tr_dev">'+
+          '<td class="info-nome td_dev">'+campo_nome.value+'</td>'+
+          '<td class="info-idade td_dev">'+campo_idade.value+'</td>'+
+          '<td class="info-time td_dev">'+campo_time.value+'</td>'+
+          '<td class="info-tec td_dev" >'+campo_tec.value+'</td>'+
+          '<td class="info-smoker td_dev">'+isSmoker(campo_smoker.value)+'</td>'+
+          '<td class="info-funcoes td_dev">'+
+            '<img class="btnIcon" src="./res/_imgs/edit.png">'+
+            '<img class="btnIcon" src="./res/_imgs/delete.png">'+
+          '</td>'+
+        '</tr>'+
+      '</table>'+
+    '</td>'+
+  '</tr>'
+  return nova_empresa;
+}
