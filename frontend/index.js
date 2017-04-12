7403f0b4448e47b95fe2fed6e55eb61bf94e59ef
luan-prates <pratesr21@gmail.com>
2017-04-12 19:33:09 -0300

    Colocando a logo da Iqnus no fundo da pagina com css.

--- frontend/index.js
+++ frontend/index.js
@@ -15,3 +15,6 @@ function criaElemento(elemento_criado, atributo1,valor1, atributo2,valor2, atrib
 
 criaElemento('script', 'src', './_js/manipula-html.js');
 criaElemento('script', 'src', './_js/adiciona-desenvolvedor.js');
+
+criaElemento('link', 'rel', 'stylesheet', 'href', '_css/index.css');
+criaElemento('link', 'rel','shortcut icon', 'href', 'res/_imgs/iqnus-favicon.ico');
