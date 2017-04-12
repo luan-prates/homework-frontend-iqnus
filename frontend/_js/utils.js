0e528715c1fc4e4bc6388ad56b6a609bc5e796e8
luan-prates <pratesr21@gmail.com>
2017-04-12 15:48:12 -0300

    Criei a função limpar campos.

---
+++ frontend/_js/utils.js
@@ -0,0 +1,16 @@
+function adicionarAtributos(identificador_elemento, atributo1, valor1, atributo2, valor2)
+{
+  document.querySelector(identificador_elemento).setAttribute(atributo1, valor1);
+  if(atributo2 != null) {
+    document.querySelector(identificador_elemento).setAttribute(atributo2, valor2);
+  }
+}
+
+function limpaCampos() {
+  campo_empresa.value = "";
+  campo_nome.value = "";
+  campo_idade.value = "";
+  campo_time.value = "";
+  campo_tec.value = "";
+  campo_smoker.value = "";
+}
