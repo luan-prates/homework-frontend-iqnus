471324fe7bdb60266ef1b82696e5b9329d20a86c
luan-prates <pratesr21@gmail.com>
2017-04-12 14:49:36 -0300

    Adicionando um função para criar elementos direto na DOM.
    Como não posso alterar o index.html então vamos manipular o DOM. Com a
    função CriaElemento() posso adicionar elementos dimamicamente.

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,14 @@
-// Seu código aqui!
+function criaElemento(elemento_criado, atributo1,valor1, atributo2,valor2, atributo3,valor3, atributo4,valor4)
+{
+  var elemento = document.createElement(elemento_criado);
+  elemento.setAttribute(atributo1, valor1);
+  elemento.setAttribute(atributo2, valor2);
+  elemento.setAttribute(atributo3, valor3);
+  elemento.setAttribute(atributo4, valor4);
+
+  if(elemento_criado == 'script' || elemento_criado == 'link' ){
+    document.getElementsByTagName("head").item(0).appendChild(elemento);
+  }else{
+    document.body.appendChild(elemento);
+  }
+}
