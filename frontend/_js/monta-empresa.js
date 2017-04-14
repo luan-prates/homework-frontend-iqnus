ca3d5d60c0c39e1172bbfdffb6e91d0ccd9da824
luan-prates <pratesr21@gmail.com>
2017-04-14 19:30:52 -0300

    adicionando id das imagens para montar as empresas

--- frontend/_js/monta-empresa.js
+++ frontend/_js/monta-empresa.js
@@ -1,4 +1,4 @@
-function montarEmpresa(nome_empresa, id_dev, campo_nome, campo_idade, campo_time, campo_tec, campo_smoker) {
+function montarEmpresa(nome_empresa, id_dev, id_edita, id_deleta, campo_nome, campo_idade, campo_time, campo_tec, campo_smoker) {
   var nova_empresa =
   '<tr class="tr_empresa">'+
     '<td class="info-empresa">'+nome_empresa+'</td>'+
@@ -11,8 +11,8 @@ function montarEmpresa(nome_empresa, id_dev, campo_nome, campo_idade, campo_time
           '<td class="info-tec td_dev" >'+campo_tec.value+'</td>'+
           '<td class="info-smoker td_dev">'+isSmoker(campo_smoker.value)+'</td>'+
           '<td class="info-funcoes td_dev">'+
-            '<img class="btn_Editar" src="./res/img/edit.png">'+
-            '<img class="btnIcon" src="./res/img/delete.png">'+
+            '<img id="'+id_edita+'"' + 'class="btn_Editar" src="./res/img/edit.png">'+
+            '<img id="'+id_deleta+'"' + 'class="btnIcon" src="./res/img/delete.png">'+
           '</td>'+
         '</tr>'+
       '</table>'+
